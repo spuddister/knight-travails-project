8e920d9c743acad8710b9fb36ce3967d0f31a5a5
Sam Puddister <sampuddister@gmail.com>
2022-10-20 16:01:17 -0400

    Using the knight factory function, I built the knightTravails function which itself builds a tree using the knights next moves until the end location is reached on one of the branches. The knightTravails function then returns the path taken which always happens to be the shortest.

--- knights-travails.js
+++ knights-travails.js
@@ -1,18 +1,9 @@
-/**
- * Pseudo Code
- * 1. Call function with start and end locations
- * 2. Initialize a knight object with start location
- *      Object includes all combinations of possible knight movement
- *      Object includes getters and setters as seen fit
- * 3. Initialize a new gameboard object with positions
- *      includes root node made with start position
- *
- */
+//Knights Travails
 
-let knight = (start, end) => {
+const knight = (start) => {
   let xPos = start[0];
   let yPos = start[1];
-  let pred;
+  let predecessor = null;
 
   const possibleMoves = [
     [1, 2],
@@ -25,23 +16,84 @@ let knight = (start, end) => {
     [-1, 2],
   ];
 
-  function nextKnights() {
-    let newKnights = possibleMoves
+  function setPred(pred) {
+    predecessor = pred;
+  }
+  function getPred() {
+    return predecessor;
+  }
+
+  function nextKnightLocations() {
+    let newKnightLoc = possibleMoves
       .map((possibleMove) => {
         //return an array of all possible new locations
         return [xPos + possibleMove[0], yPos + possibleMove[1]];
       })
       .filter((newLocation) => {
         //filter the array for all locations that aren't on the board
-        0 <= newLocation[0] &&
-          newLocation[0] < 8 &&
-          0 <= newLocation[1] &&
-          newLocation[1] < 8;
+        return (
+          1 <= newLocation[0] &&
+          newLocation[0] < 9 &&
+          1 <= newLocation[1] &&
+          newLocation[1] < 9
+        );
       });
-    return newKnights;
+    return newKnightLoc;
   }
+
+  function position() {
+    return `[${xPos},${yPos}]`;
+  }
+
+  return {
+    nextKnightLocations,
+    setPred,
+    getPred,
+    position,
+  };
 };
 
-function buildGameBoard(start, end) {}
+function knightTravails(start, end) {
+  if (
+    !(1 <= start[0] && start[0] < 9 && 1 <= start[1] && start[1] < 9) ||
+    !(1 <= end[0] && end[0] < 9 && 1 <= end[1] && end[1] < 9)
+  ) {
+    return console.log(
+      "Please enter coordinates between 1 and 8 like on a chess board. E.g. [1,8] or [3,5]."
+    );
+  }
 
-// function newSquare
+  console.log(`> knightTravails([${start}],[${end}])`);
+
+  let rootKnight = knight(start);
+  let endKnight = knight(end);
+  let queue = [rootKnight];
+
+  while (endKnight.position() != queue[0].position()) {
+    let currentKnight = queue.shift();
+    //From the current knight location, get all possible next moves and turn each into a knight piece. Then set their predecessor as the current knight location and add them to the queue.
+    let nextKnights = currentKnight.nextKnightLocations().map((newPos) => {
+      k = knight(newPos);
+      k.setPred(currentKnight);
+      return k;
+    });
+    queue.push(...nextKnights);
+  }
+
+  //Track back from the current knight to find the fastest route taken
+  let currentKnight = queue[0];
+  let path = [currentKnight.position()];
+
+  while (path[0] != `[${start.toString()}]`) {
+    currentKnight = currentKnight.getPred();
+    path.unshift(currentKnight.position());
+  }
+
+  //Print out the route
+  console.log(`=> You made it in ${path.length - 1} moves! Here's your path:`);
+  for (let i = 0; i < path.length; i++) {
+    console.log(path[i].padStart(7));
+  }
+}
+
+module.exports = knightTravails;
