64ab3a974bee15ce8bdee9a6e65c907c96e7f8cc
Sam Puddister <sampuddister@gmail.com>
2022-10-20 16:02:33 -0400

    Created a test file to run the knightTravails function and display its output.

---
+++ knights-travails-TEST.js
@@ -0,0 +1,41 @@
+#!/usr/bin/node
+
+const knightTravails = require("./knights-travails");
+
+//TEST RUNS
+
+knightTravails([2, 2], [4, 4]);
+/** OUTPUT
+ *
+ * > knightTravails([3,6],[1,1])
+ * => You made it in 3 moves! Here's your path:
+ *  [3,6]
+ *  [4,4]
+ *  [3,2]
+ *  [1,1]
+ */
+
+knightTravails([3, 6], [1, 1]);
+/** OUTPUT
+ *
+ * > knightTravails([2,2],[4,4])
+ * => You made it in 4 moves! Here's your path:
+ *   [2,2]
+ *   [3,4]
+ *   [4,6]
+ *   [6,5]
+ *   [4,4]
+ */
+
+knightTravails([7, 1], [1, 8]);
+/** OUTPUT
+ *
+ * > knightTravails([7,1],[1,8])
+ * => You made it in 5 moves! Here's your path:
+ *   [7,1]
+ *   [8,3]
+ *   [6,4]
+ *   [4,5]
+ *   [2,6]
+ *   [1,8]
+ */
